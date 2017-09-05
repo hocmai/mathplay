60a152b9bebbb6146d48cc1e84cf1213c6792137
Tan Nguyen <tantanb2@gmail.com>
2017-09-05 09:06:03 +0700

    add git ignore

--- app/views/site/questions/ChonMauSacPhuHop.blade.php
+++ app/views/site/questions/ChonMauSacPhuHop.blade.php
@@ -5,12 +5,12 @@ $answer = array_rand($colors, rand(4,6));
 ?>
 
 <div class="start">
-	{{ 'Trong ô thứ '.($answer[0] + 1).' có màu gì?' }}
+	{{ 'Trong ô thứ '.($answer[3] + 1).' có màu gì?' }}
 </div>
 
 <div class="container-fluid question-wrapper">
 	{{ Form::open(['method' => 'GET', 'class' => 'answer-question-form', 'id' => 'question-'.$question->id]) }}
-		<input type="hidden" name="true_answer" value="{{ $colors[$answer[0]][0] }}" />
+		<input type="hidden" name="true_answer" value="{{ $colors[$answer[3]][0] }}" />
 		<input type="hidden" name="qid" value="{{ $question->id }}" />
 		<input type="hidden" name="lession_id" value="{{ !empty($lession->id) ? $lession->id : '' }}" />
 		<input type="hidden" name="question_number" value="{{ $question_num }}" />
