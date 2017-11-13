0a5898425404db6196f0ad26ad3575dce6241009
Tan Nguyen <tantanb2@gmail.com>
2017-11-13 18:27:15 +0700

    Them phep tinh tong cho Bai Toan Cai Can

--- app/views/site/questions/BaiToanCaiCan/form.blade.php
+++ app/views/site/questions/BaiToanCaiCan/form.blade.php
@@ -1,4 +1,20 @@
 <div class="form-group">
-	{{ Form::label('', 'Form options') }}
-	{{ Form::text('question_config[option][]', !empty($config['option']) ? $config['option'] : '', ['class' => 'form-control']) }}
+	{{ Form::label('', 'Dạng câu hỏi') }}
+	{{ Form::select('question_config[type][]', [
+		'' => 'Mặc định',
+		'coin' => 'Thả đồng xu',
+		'basic' => 'Chọn đáp án'
+	], !empty($config['type']) ? $config['type'] : '', ['class' => 'form-control']) }}
+</div>
+<div class="form-group">
+	<div class="row">
+		<div class="col-xs-12 col-sm-6">
+			<label>Nhỏ nhất</label>
+			{{ Form::number('question_config[min][]', !empty($config['min']) ? $config['min'] : 1, ['class'=>'form-control']) }}
+		</div>
+		<div class="col-xs-12 col-sm-6">
+			<label>Nhỏ nhất</label>
+			{{ Form::number('question_config[max][]', !empty($config['max']) ? $config['max'] : 100, ['class'=>'form-control']) }}
+		</div>
+	</div>
 </div>
