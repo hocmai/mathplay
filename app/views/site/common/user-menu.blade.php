ca20907d3ea08d2dcb6158946973a9c90b50b75e
Tan Nguyen <tantanb2@gmail.com>
2018-01-01 21:37:11 +0700

    finished subject & grade page with new design 1.1

--- app/views/site/common/user-menu.blade.php
+++ app/views/site/common/user-menu.blade.php
@@ -2,15 +2,15 @@
     <div class="box-info">
         <div class="text">
             <div class="avatar">
-                <img src="{{ asset('frontend/images/avata.jpg') }}" class="img-responsive mauto" alt="">
+                <img src="{{ Common::getUserAvatar() }}" class="img-responsive mauto" alt="">
             </div>
             <div class="btn-group" role="group">
                 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    {{ Auth::user()->get()->username }}
+                    {{ Common::getUserName() }}
                     <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu">
-                    <li>{{ renderUrl('SiteMemberController@history', 'Lịch sử làm bài', ['uid' => Auth::user()->get()->id]) }}</li>
+                    <li>{{ renderUrl('SiteMemberController@history', 'Lịch sử làm bài', ['uid' => Common::getObject(Auth::user()->get(), 'id')]) }}</li>
                     <li>{{ renderUrl('SiteUserController@logout', 'Thoát') }}</li>
                 </ul>
             </div>
