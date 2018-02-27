ac71a36fb51cb6e26dadbfe7207569e3035c891e
Dang NV <nguyenminhdang1994@gmail.com>
2018-02-27 15:02:40 +0700

    huong dan giai do do dai doan thang

--- app/views/site/questions_guide/tong_cac_nhom_hinh.blade.php
+++ app/views/site/questions_guide/tong_cac_nhom_hinh.blade.php
@@ -1,4 +1,4 @@
-<div class="huong-dan-giai text-left" style="display: block;" >
+<div class="huong-dan-giai text-left" >
 	<h2>Hướng dẫn giải</h2>
 	<div class="wrapper">
 		<div class="question-content">
@@ -20,11 +20,11 @@
 			<p>-Có tất cả {{ $group }} nhóm, mỗi nhóm {{ $each }} {{ $imageShow }}.</p>
 			<p class="answers">Viết biểu thức nhân biểu diễn {{ $group.' x '.$each.' = '.($group*$each) }}</p>
 		@elseif($type=='phan-tich')
-			<p>Có <b>{{ $group }}</b> nhóm<b> {{ $imageShow }}</b></p>
+			<p>-Có <b>{{ $group }}</b> nhóm<b> {{ $imageShow }}</b></p>
 			<p class="answers">Đáp án {{ $group.' x '.$each.' = '.($group*$each) }}</p>
 		@else
-			<p>Có {{ $group }} Nhóm {{ $imageShow }}</p>
-			<p>Có {{ $each }} {{ $imageShow }} trong mỗi nhóm</p>
+			<p>-Có {{ $group }} Nhóm {{ $imageShow }}</p>
+			<p>-Có {{ $each }} {{ $imageShow }} trong mỗi nhóm</p>
 
 		@endif
 		<button class="btn lam-bai-tiep margin0" data-dismiss="modal" aria-label="Close">Làm bài tiếp</button>
