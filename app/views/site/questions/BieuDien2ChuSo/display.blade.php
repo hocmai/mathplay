7e87f9e3cfb7d92fba08b363bc7e3c9ae154a2fa
Tan Nguyen <tantanb2@gmail.com>
2018-03-02 14:47:30 +0700

    Add huong dan giai bieu dien 2 chu so

--- app/views/site/questions/BieuDien2ChuSo/display.blade.php
+++ app/views/site/questions/BieuDien2ChuSo/display.blade.php
@@ -2,130 +2,78 @@
 $type = !empty($config['type']) ? $config['type'] : getRandArrayVal(['input', 'choose']);
 $max = !empty($config['max']) ? $config['max'] : 100;
 $img = getRandArrayVal(CommonQuestion::getRandImg('BieuDien2ChuSo'));
-$num1 = $answer = rand(1, $max);
+$num1 = $answer = rand(11, $max);
 $num2 = $num1+rand(-5, 5);
 
+$str_arr = [];
 if( $type == 'choose' ){
-	$answer = getRandArrayVal([$num1, $num2]);
-	if( $num2 == $num1 ){
-		$num2++;
-	}
-}
-?>
-<div class="start">
-	<?php 
-	$str_arr = ['Hình nào dưới đây biểu diễn số', $answer];
-	$title = $str_arr[0].' '.$answer;
-	$title_slug = Str::slug($str_arr[0], '').'_'.$answer;
-	?>
-	<script type="text/javascript">
-		@foreach( $str_arr as $key => $str )
-		if( !checkIdExist('{{ Str::slug($str, '') }}') ){
-			audioList.push({id: '{{Str::slug($str, '')}}', url: '{{ CommonQuestion::getAudioPath($str) }}' });
-		}
-		@endforeach
-	</script>
-	<div class="play-question-sound">
-		<button class="control play" onclick="return PlaySoundManage(this, '{{ $title_slug }}' );"></button>
-	</div>
-	{{ $title }}
-</div>
+    $answer = getRandArrayVal([$num1, $num2]);
+    if( $num2 == $num1 ){
+        $num2++;
+    }
+    $str_arr = ['Hình nào dưới đây biểu diễn số', $answer];
+}?>
+
+@include('site.questions.render-title', ['question' => $question, 'str_arr' => $str_arr ] )
 
 <div class="container-fluid question-wrapper">
-	{{ Form::open(['method' => 'GET', 'class' => 'answer-question-form', 'id' => 'question-'.$question->id]) }}
-		<input type="hidden" name="true_answer" value="{{ $answer }}" />
-		<input type="hidden" name="qid" value="{{ $question->id }}" />
-		<input type="hidden" name="lession_id" value="{{ !empty($lession->id) ? $lession->id : '' }}" />
-		<input type="hidden" name="question_number" value="{{ $question_num }}" />
-		
-		<div class="form-group">
-			<div class="content inline-block">
-				<div class="numbers">
-					@if($type == 'input')
-						<div class="number">
-							@for($i = 1; $i <= floor($num1/10); $i++)
-								<div class="hang-chuc">
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-									<span class="item" style="background-image: url('{{ $img }}')"></span>
-								</div>
-							@endfor
-							@if( $num1 > floor($num1/10)*10 )
-								<div class="hang-dv">
-									@for($i = 1; $i <= ($num1 - floor($num1/10)*10); $i++)
-										<span class="item" style="background-image: url('{{ $img }}')"></span>
-									@endfor
-								</div>
-							@endif
-						</div>
-						{{ Form::text('answer', '', ['class'=>'form-control', 'style'=>'width:65px']) }}
-					@else
-						<div class="form-group radio">
-							{{ Form::radio('answer', $num1, false, ['id'=>$question_num.'-1']) }}
-							<label for="{{ $question_num }}-1">
-								<div class="number">
-									@for($i = 1; $i <= floor($num1/10); $i++)
-										<div class="hang-chuc">
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-										</div>
-									@endfor
-									@if( $num1 > floor($num1/10)*10 )
-										<div class="hang-dv">
-											@for($i = 1; $i <= ($num1 - floor($num1/10)*10); $i++)
-												<span class="item" style="background-image: url('{{ $img }}')"></span>
-											@endfor
-										</div>
-									@endif
-								</div>
-							</label>
-						</div>
-						<div class="form-group radio">
-							{{ Form::radio('answer', $num2, false, ['id'=>$question_num.'-2']) }}
-							<label for="{{ $question_num }}-2">
-								<div class="number">
-									@for($i = 1; $i <= floor($num2/10); $i++)
-										<div class="hang-chuc">
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-											<span class="item" style="background-image: url('{{ $img }}')"></span>
-										</div>
-									@endfor
-									@if( $num2 > floor($num2/10)*10 )
-										<div class="hang-dv">
-											@for($i = 1; $i <= ($num2 - floor($num2/10)*10); $i++)
-												<span class="item" style="background-image: url('{{ $img }}')"></span>
-											@endfor
-										</div>
-									@endif
-								</div>
-							</label>
-						</div>
-					@endif
-				</div>
-			</div>
-		</div>
-	{{ Form::close() }}
+    {{ Form::open(['method' => 'GET', 'class' => 'answer-question-form', 'id' => 'question-'.$question->id]) }}
+        <input type="hidden" name="true_answer" value="{{ $answer }}" />
+        <input type="hidden" name="qid" value="{{ $question->id }}" />
+        <input type="hidden" name="lession_id" value="{{ !empty($lession->id) ? $lession->id : '' }}" />
+        <input type="hidden" name="question_number" value="{{ $question_num }}" />
+        
+        <div class="form-group">
+            <div class="content inline-block">
+                <div class="numbers">
+                    @if($type == 'input')
+                        <div class="number">
+                            @for($i = 1; $i <= floor($num1/10); $i++)
+                                <div class="hang-chuc">
+                                    @for( $k = 1; $k <= 10; $k++ )
+                                        <span class="item" style="background-image: url('{{ $img }}')"></span>
+                                    @endfor
+                                </div>
+                            @endfor
+                            @if( $num1 > floor($num1/10)*10 )
+                                <div class="hang-dv">
+                                    @for($i = 1; $i <= ($num1 - floor($num1/10)*10); $i++)
+                                        <span class="item" style="background-image: url('{{ $img }}')"></span>
+                                    @endfor
+                                </div>
+                            @endif
+                        </div>
+                        {{ Form::text('answer', '', ['class'=>'form-control inline-block', 'style'=>'width:65px']) }}
+                    @else
+                        @for( $i = 1; $i <=2; $i++ )
+                            <?php $num = ($i == 1) ? $num1 : $num2; ?>
+                            <div class="form-group radio">
+                                {{ Form::radio('answer', $num, false, ['id' => $question_num.'-'.$i]) }}
+                                <label for="{{ $question_num.'-'.$i }}">
+                                    <div class="number">
+                                        @for($j = 1; $j <= floor($num/10); $j++)
+                                            <div class="hang-chuc">
+                                                @for( $k = 1; $k <= 10; $k++ )
+                                                    <span class="item" style="background-image: url('{{ $img }}')"></span>
+                                                @endfor
+                                            </div>
+                                        @endfor
+                                        @if( $num > floor($num/10)*10 )
+                                            <div class="hang-dv">
+                                                @for($j = 1; $j <= ($num - floor($num/10)*10); $j++)
+                                                    <span class="item" style="background-image: url('{{ $img }}')"></span>
+                                                @endfor
+                                            </div>
+                                        @endif
+                                    </div>
+                                </label>
+                            </div>
+                        @endfor
+                    @endif
+                </div>
+            </div>
+        </div>
+    {{ Form::close() }}
 </div>
+
+@include('site.questions_guide.bieu_dien_2_chu_so')
---
+++ app/views/site/questions_guide/bieu_dien_2_chu_so.blade.php
@@ -0,0 +1,54 @@
+<div class="huong-dan-giai text-left" style="display: block;">
+    <h2>Hướng dẫn giải</h2>
+    <div class="wrapper">
+        <p>Một cột đầy đủ có 10 ô, đếm theo hàng chục và cộng với cột cuối cùng</p>
+        @if( $type == 'choose' )
+            <div class="numbers">
+                <div class="form-group radio padding0">
+                    <label style="color: black">
+                        <div class="number">
+                            @for($j = 1; $j <= floor($answer/10); $j++)
+                                <div class="hang-chuc">
+                                    <h4 class="text-center"><b>{{ $j*10 }}</b></h4>
+                                    @for( $k = 1; $k <= 10; $k++ )
+                                        <span class="item" style="background-image: url('{{ $img }}')"></span>
+                                    @endfor
+                                </div>
+                            @endfor
+                            @if( $answer > floor($answer/10)*10 )
+                                <div class="hang-dv">
+                                    <h4 class="text-center"><b>+{{ $answer - floor($answer/10)*10 }}</b></h4>
+                                    @for($j = 1; $j <= ($answer - floor($answer/10)*10); $j++)
+                                        <span class="item" style="background-image: url('{{ $img }}')">{{ $j }}</span>
+                                    @endfor
+                                </div>
+                            @endif
+                        </div>
+                    </label>
+                </div>
+            </div>
+        @else
+            <div class="number">
+                @for($i = 1; $i <= floor($num1/10); $i++)
+                    <div class="hang-chuc">
+                        <h4 class="text-center"><b>+{{ $i*10 }}</b></h4>
+                        @for( $k = 1; $k <= 10; $k++ )
+                            <span class="item" style="background-image: url('{{ $img }}')"></span>
+                        @endfor
+                    </div>
+                @endfor
+                @if( $num1 > floor($num1/10)*10 )
+                    <div class="hang-dv">
+                        <h4 class="text-center"><b>+{{ $num1 - floor($num1/10)*10 }}</b></h4>
+                        @for($i = 1; $i <= ($num1 - floor($num1/10)*10); $i++)
+                            <span class="item" style="background-image: url('{{ $img }}')"></span>
+                        @endfor
+                    </div>
+                @endif
+            </div>
+            <div class="clear clearfix"></div>
+        @endif
+        <p class="answers">Đáp án đúng là <b>{{ $answer }}</b></p>
+        <button class="btn lam-bai-tiep margin0" data-dismiss="modal" aria-label="Close">Làm bài tiếp</button>
+    </div>
+</div>
