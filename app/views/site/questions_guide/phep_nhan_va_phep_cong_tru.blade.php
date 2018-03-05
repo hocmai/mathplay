4997b2ff4f3c1067be73b3f2ece31dd17a55e499
Tan Nguyen <tantanb2@gmail.com>
2018-03-05 15:07:21 +0700

    add huong dan giai PhepNhanVaPhepCongTru

--- app/views/site/questions_guide/phep_nhan_va_phep_cong_tru.blade.php
+++ app/views/site/questions_guide/phep_nhan_va_phep_cong_tru.blade.php
@@ -1,4 +1,4 @@
-<div class="huong-dan-giai text-left" style="display: block">
+<div class="huong-dan-giai text-left">
 	<h2>Hướng dẫn giải</h2>
 	<div class="wrapper">
 		<p>Ta thực hiện phép nhân trước: {{ $a.' x '.$b.' = '.($a*$b) }}</p>
