b48e9b49f5a135a350c7bdd9a0cab3090e749d02
Dang NV <nguyenminhdang1994@gmail.com>
2018-03-09 09:13:40 +0700

    add edit huong dan giai do do dai doan thang

--- app/views/site/questions_guide/do_do_dai_doan_thang.blade.php
+++ app/views/site/questions_guide/do_do_dai_doan_thang.blade.php
@@ -2,6 +2,12 @@
 	<h2>Hướng dẫn giải</h2>
 	<div class="wrapper">
 		<div class="content">
+			<p>*Nhớ:</p>
+			<p>Các bước sử dụng thước kẻ đo độ dài đoạn thẳng:</p>
+			<p>-Đặt thước kẻ dọc theo đoạn thẳng, vạch số 0 trùng với một đầu đoạn thẳng</p>
+			<p>-Nhìn vào vạch gần nhất với đầu kia của đoạn thẳng. Đó chính là số đo chiều dài của đoạn thẳng.</p>
+			<p>-Trên mặt thước kẻ, đều có vạch chia độ dài. Khi đo một vật, ta biết được chiều dài của vật bằng cách đọc số đo trên vạch chia.</br>
+			-Luôn luôn quan sát thật cẩn thận các vạch để chắc chắn đơn vị mà mỗi vạch thể hiện.</p>
 			<div class="work-area inline-block form-group">
 				<div class="line-wrapper"><span class="line" style="width: {{ $length*10  }}%"></span></div>
 				<div class="sort-area">
@@ -9,10 +15,10 @@
 				</div>
 			</div>
 			<div class="clearfix"></div>
-			<p>-Đặt thước kẻ dọc theo đoạn thẳng, vạch số 0 trùng với một đầu đoạn thẳng</p>
-			<p>-Nhìn vào vạch gần nhất với đầu kia của đoạn thẳng. Đó chính là số đo chiều dài của đoạn thẳng.</p>
-			<p>-Vạch đầu tiên (vạch 0) đến vạch số {{ $length }} cm. Đó chính là số đo chiều dài của đoạn thẳng</p>
-			<p class="answers">Đoạn thẳng trên dài {{ $length }} cm</p>		
+			<p>Giải:</p>
+			<p>-Đặt thước kẻ dọc theo đoạn thẳng, vạch số 0 trùng với một đầu đoạn thẳng.<br>
+			-Nhìn vào vạch gần nhất với đầu kia của đoạn thẳng chỉ số <b>{{ $length }}</b>. Đó chính là số đo chiều dài của đoạn thẳng.</p>
+			<p class="answers">kết luận: Đoạn thẳng trên dài <b>{{ $length }}</b> cm</p>		
 		</div>	
 		<button class="btn lam-bai-tiep margin0" data-dismiss="modal" aria-label="Close">Làm bài tiếp</button>
 	</div>
