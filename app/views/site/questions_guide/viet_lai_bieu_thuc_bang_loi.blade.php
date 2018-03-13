45e71612be205436bdea15be6deed51b80d96499
Dang NV <nguyenminhdang1994@gmail.com>
2018-03-13 13:44:19 +0700

    edit hdg viet lai bieu thuc bang loi

--- app/views/site/questions_guide/viet_lai_bieu_thuc_bang_loi.blade.php
+++ app/views/site/questions_guide/viet_lai_bieu_thuc_bang_loi.blade.php
@@ -8,7 +8,7 @@
 			<p>{{ ' = '.(floor($num/10)-3).' chục '.' + '.(($num-(floor($num/10)*10))+30).' đơn vị' }}</p>
 		@else
 		@endif
-		<p class="answers">đáp án đúng là:<b> {{ (floor($num/10) > 1 ) ? (floor($num/10)-1).' chục cộng '.( $num - ((floor($num/10)-1)*10) ).' đơn vị' : floor($num/10).' chục cộng '.($num-(floor($num/10)*10)).' đơn vị' }}</b></p>
+		<p class="answers">Đáp án đúng là:<b> {{ (floor($num/10) > 1 ) ? (floor($num/10)-1).' chục cộng '.( $num - ((floor($num/10)-1)*10) ).' đơn vị' : floor($num/10).' chục cộng '.($num-(floor($num/10)*10)).' đơn vị' }}</b></p>
 		<button class="btn lam-bai-tiep margin0" data-dismiss="modal" aria-label="Close">Làm bài tiếp</button>
 	</div>
 </div>
