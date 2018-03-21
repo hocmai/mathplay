9135c1b4c014c3309703db04b8f09781393b776d
Dang NV <nguyenminhdang1994@gmail.com>
2018-03-21 15:08:04 +0700

    add xac dinh so chan le tren tia so va huong dan giai, edit hdg phep nhan moi so

--- app/views/site/questions/XacDinhChanLeTrenTiaSo/display.blade.php
+++ app/views/site/questions/XacDinhChanLeTrenTiaSo/display.blade.php
@@ -1,48 +1,45 @@
 <?php
+$true_answer = '';
 $min = !empty($config['min_value']) ? $config['min_value'] : rand(1,10);
-$plus = !empty($config['number_plus']) ? $config['number_plus'] : rand(1,10);
-$range = (!empty($config['number_count']) && $config['number_count'] >= 3) ? $config['number_count'] : rand(5, 10);
+$type = getRandArrayVal(['even','odd']);
+$range = (!empty($config['number_count']) && $config['number_count'] >= 3) ? $config['number_count'] : 10;
 $lines = [];
+$answer_arr = [];
 for($i = 0; $i < $range; $i++){
-	$lines[] = $min;
-	$min += $plus;
-}
-$type = !empty($config['answer_type']) ? $config['answer_type'] : getRandArrayVal(['input','inline']);
-$position = 0;
-if($type == 'inline'){
-	$position = array_rand($lines, 4);
-	$answer = getRandArrayVal($lines);
-	
- } else{
-
+    $lines[] = $min;
+    if($type == 'even' && $min%2 == 0){
+        $true_answer .= $min;
+        $answer_arr[] = $min;
+    }elseif($type == 'odd' && $min%2 > 0) {
+        $true_answer .= $min;
+        $answer_arr[] = $min;
+    }                                                               
+    $min ++;
 }
 
-if($type == 'input'){
-	$str = ['Chọn các số lẻ ở trên tia số?'];
-}else {
-	$str = ['Chọn các số chẵn ở trên tia số?'];
-}
+// even la chan 
+// odd la le
 ?>
 
-@include('site.questions.render-title', ['question' => $question, 'str' => $str])
+@include('site.questions.render-title', ['question' => $question, 'str_arr' => ['Chọn các số ', ($type == 'even') ? ' chẵn ' : ' lẻ ' ,'trên tia số ' ]])
 
 <div class="container-fluid question-wrapper">
-	{{ Form::open(['method' => 'GET', 'class' => 'answer-question-form', 'id' => 'question-'.$question->id]) }}
-		<input type="hidden" name="true_answer" value="  " />
-		<input type="hidden" name="qid" value="{{ $question->id }}" />
-		<input type="hidden" name="lession_id" value="{{ !empty($lession->id) ? $lession->id : '' }}" />
-		<input type="hidden" name="question_number" value="{{ $question_num }}" />
-		<input type="hidden" name="answer" />
-		
-		<div class="form-group number-line">
-			<div class="content inline-block">
-				@foreach($lines as $key => $value)
-					<div class="line inline-block" >
-						{{ Form::checkbox('answer_number') }}
-						<label>{{ $value }}</label>
-					</div>
-				@endforeach
-			</div>
-		</div>
-	{{ Form::close() }}
+    {{ Form::open(['method' => 'GET', 'class' => 'answer-question-form', 'id' => 'question-'.$question->id]) }}
+        <input type="hidden" name="true_answer" value="{{ $true_answer }}" />
+        <input type="hidden" name="qid" value="{{ $question->id }}" />
+        <input type="hidden" name="lession_id" value="{{ !empty($lession->id) ? $lession->id : '' }}" />
+        <input type="hidden" name="question_number" value="{{ $question_num }}" />
+        <input type="hidden" name="answer" />
+        <div class="form-group number-line">
+            <div class="content inline-block">
+                @foreach($lines as $key => $value)
+                    <div class="line inline-block" >
+                        {{ Form::checkbox('answer_number', $value, false,['id' => $question_num.$key.'oddeven'] )}}
+                        <label for="{{ $question_num.$key.'oddeven' }}">{{ $value }}</label>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    {{ Form::close() }}
 </div>
+@include('site.questions_guide.xac_dinh_chan_le_tren_tia_so')
