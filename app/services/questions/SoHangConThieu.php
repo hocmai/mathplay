abd9b2f22ffc1ef6b8f7d7a0a41ce3ae20344ccd
Dang NV <nguyenminhdang1994@gmail.com>
2018-03-29 10:41:49 +0700

    sua  trong app/services/questions/SoHangConThieu.php .->Tìm số hạng còn thiếu trong phép cộng và phép trừ

--- app/services/questions/SoHangConThieu.php
+++ app/services/questions/SoHangConThieu.php
@@ -1,7 +1,7 @@
 <?php
 Class SoHangConThieu extends CommonQuestion{
 
-	protected static $title = 'Tìm số hạng còn thiếu trong phép cộng';
+	protected static $title = 'Tìm số hạng còn thiếu trong phép cộng và phép trừ';
 	public static function getTitle(){
 		return self::$title;
 	}
