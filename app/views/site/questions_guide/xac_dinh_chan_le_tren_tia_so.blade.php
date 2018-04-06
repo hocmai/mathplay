a2c8b0e7ceedecd9c701440cc3f214c592fa0de8
Dang NV <nguyenminhdang1994@gmail.com>
2018-04-06 08:57:11 +0700

    sưa text xac dinh so chan le, va xac dinh so chan le tren tia so, gioi han max =20 trong xac dinh so chan le

--- app/views/site/questions_guide/xac_dinh_chan_le_tren_tia_so.blade.php
+++ app/views/site/questions_guide/xac_dinh_chan_le_tren_tia_so.blade.php
@@ -2,8 +2,11 @@
     <h2>Hướng dẫn giải</h2>
     <div class="wrapper">
     	<p>* Nhớ<br>
-			Ở trên tia số, ta đi tìm các số có chữ số tận cùng là 1, 3, 5, 7, hoặc 9. (đối với trường hợp số lẻ)<br>
-			Ở trên tia số, ta đi tìm các số có chữ số tận cùng là 0, 2, 4, 6, hoặc 8. (đối với trường hợp số chẵn)
+			Số chẵn là số có chữ số tận cùng là 2, 4, 6, 8, hoặc 0.<br>
+			Số lẻ là số có chữ số tận cùng là 1, 3, 5, 7, hoặc 9.
+		</p>
+		<p>*Giải<br>
+			{{ ($type == 'even') ? 'Ở trên tia số, ta đi tìm các số có chữ số tận cùng là 0, 2, 4, 6, hoặc 8.': 'Ở trên tia số, ta đi tìm các số có chữ số tận cùng là 1, 3, 5, 7, hoặc 9. '}}
 		</p>
     	<div class="form-group number-line">
 			<div class="content inline-block">
@@ -21,7 +24,7 @@
 				@endforeach
 			</div>
 		</div>
-		<p class="answers">Như vậy, các số {{  ($type == 'even') ? ' chẵn ' : ' lẻ '}} ở trên tia số trên là {{ implode(', ', $answer_arr) }}</p> 
+		<p class="answers">Như vậy, các số {{ ( $type =='even' ) ? 'chẵn':'lẻ' }} ở trên tia số trên là {{ implode(', ', $answer_arr) }}. </p> 
         <button class="btn lam-bai-tiep margin0" data-dismiss="modal" aria-label="Close">Làm bài tiếp</button>
     </div>
 </div>
