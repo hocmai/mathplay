24ae4aa3d866ff3c5bd99edc01a152c0643df9b2
Dang NV <nguyenminhdang1994@gmail.com>
2018-04-19 14:35:25 +0700

    fix

--- app/views/site/questions_guide/phep_chia_2_so.blade.php
+++ app/views/site/questions_guide/phep_chia_2_so.blade.php
@@ -1,6 +1,7 @@
 <div class="huong-dan-giai text-left" >
 	<h2>Hướng dẫn giải</h2>
 	<div class="wrapper">
+		{{-- // phep chia 2 hinh anh --}}
 		@if($type == 'chia_img')
 			<p>-Hình trên có tất cả {{ $group }} nhóm, mỗi nhóm {{ $each }} {{ $imageShow }}.</p>
 			<div class="question-content">
@@ -20,6 +21,7 @@
 			</div>
 			<p>-Quan sát thấy {{ $each.' '.$imageShow.' trong mỗi nhóm.' }}</p>
 			<p class="answers">Phép chia được biểu diễn bởi hình đã cho là: {{ ($group*$each).' : '.$group.' = '.$each }}</p>
+			{{-- moi quan he giua phep nhan va phep chia --}}
 		@elseif($type == 'nhan-chia')
 			<p>* Mối quan hệ giữa phép nhân và phép chia.<br>
 				Cả phép nhân và phép chia đều được biểu diễn bởi một hình. Do đó, các số hạng trong biểu thức nhân và biểu thức chia trong trường hợp này là giống nhau hay phép nhân và phép chia là 2 phép tính ngược nhau.
@@ -42,12 +44,13 @@
 			<p>-Hình trên có tất cả {{ $group }} nhóm, mỗi nhóm {{ $each }} {{ $imageShow }}.</p>
 			<p>{{ $group.' x '.$each.' = '.($group*$each) }}</p>
 			<p>{{ ($group*$each).' : '.$group.' = '.$answer }}</p>
-			<p class="answers"> Như vậy số cần điền là : {{ $answer }}</p>	
+			<p class="answers"> Như vậy số cần điền là : {{ $answer }}</p>
+			{{-- phep chia 2 so chia het --}}
 		@else
 			<p>Theo bảng chia {{ $a }} ta có</p>
 			<div class=" inline-block text-center" style="border:2px solid #eee">
 				@for ($x = 1; $x <= 12; $x++)
-					<p class=" item">{{ ($x*$a).' : '.$a.' = '.(($x*$a)/$a) }}</p>
+					<p class="{{ ($b == 0) ? 'active': 'item' }}">{{ ($x*$a).' : '.$a.' = '.(($x*$a)/$a) }}</p>
 				@endfor
 			</div>
 			<div class="clear clear-fix"></div>
